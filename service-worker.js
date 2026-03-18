48faead6e7081168e35f79e08b453dce88ab4eb1
Mohasan Uddin Tutul <mohasan.meal@gmail.com>
2026-03-18 16:27:12 +0600

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -7,6 +7,8 @@ const urlsToCache = [
   './resources/ol.js',
   './resources/ol.css',
   './resources/qgis2web.js',
+  './resources/ol-layerswitcher.js',
+  './resources/photon-geocoder-autocomplete.min.js',
 
   './layers/layers.js',
   './layers/FacilityInfo_4.js',
@@ -16,16 +18,23 @@ const urlsToCache = [
   './FIVDB.jpg'
 ];
 
+// Install
 self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Caching files...');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
+// Fetch
 self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then(response => {
+        return response || fetch(event.request);
+      })
   );
 });
